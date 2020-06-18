cf796357115492d62077fd169d668baec54569f3
flboy1758 <flboy1758@gmail.com>
2020-06-18 13:13:00 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,27 @@
 // Import stylesheets
 import './style.css';
 
+class TetrisBoard {
+  private context: CanvasRenderingContext2D;
+  constructor(context: CanvasRenderingContext2D) {
+    this.context = context;
+  }
+
+  draw(): void {
+    context.lineCap = 'square';
+    context.lineJoin = 'round';
+    context.strokeStyle = 'white';
+    context.lineWidth = 1;
+
+    context.rect(10,10,280, 480);
+    context.stroke();
+  }
+}
+
 const canvas = <HTMLCanvasElement> document.getElementById("game-canvas");
 
 const context = canvas.getContext("2d");
+
 
 context.lineCap = 'round';
 context.lineJoin = 'round';
@@ -12,5 +30,8 @@ context.lineWidth = 1;
 
 context.fillStyle = 'green'
 
-context.fillRect(10,10,280,100)
+context.fillRect(10,10,280,1)
 context.fill();
+
+const tetrisBoard = new TetrisBoard(context);
+tetrisBoard.draw();
